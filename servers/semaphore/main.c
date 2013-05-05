576ce303adfe99df7c7b5f46c3d92cb82db46c7d
Shervin Shaikh <shervinater@gmail.com>
2013-05-04 21:58:01 -0700

    removing and adding includes

--- servers/semaphore/main.c
+++ servers/semaphore/main.c
@@ -1,21 +1,5 @@
-#include <minix/keymap.h>
 #include <minix/callnr.h>
-#include <minix/com.h>
-#include <minix/ds.h>
-#include <minix/type.h>
-#include <minix/endpoint.h>
-#include <minix/minlib.h>
-#include <minix/type.h>
-#include <minix/vm.h>
-#include <minix/crtso.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/resource.h>
-#include <sys/utsname.h>
-#include <string.h>
-#include <machine/archtypes.h>
-#include <env.h>
+#include <stdio.h>
 
 /*===========================================================================*
  *				do_semaphore				             *
