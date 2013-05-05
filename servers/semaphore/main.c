49c584e35a3015a8b76521b58ff6c55c7c247b21
Shervin Shaikh <shervinater@gmail.com>
2013-05-04 21:55:26 -0700

    too many includes

--- servers/semaphore/main.c
+++ servers/semaphore/main.c
@@ -17,10 +17,6 @@
 #include <machine/archtypes.h>
 #include <env.h>
 
-#include "kernel/const.h"
-#include "kernel/config.h"
-#include "kernel/proc.h"
-
 /*===========================================================================*
  *				do_semaphore				             *
  *===========================================================================*/
