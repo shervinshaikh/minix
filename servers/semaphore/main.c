5c610a7f937a28338b398b4e65fc1fc4c9075d3f
Shervin Shaikh <shervinater@gmail.com>
2013-05-04 21:54:08 -0700

    forgot includes

--- servers/semaphore/main.c
+++ servers/semaphore/main.c
@@ -1,3 +1,26 @@
+#include <minix/keymap.h>
+#include <minix/callnr.h>
+#include <minix/com.h>
+#include <minix/ds.h>
+#include <minix/type.h>
+#include <minix/endpoint.h>
+#include <minix/minlib.h>
+#include <minix/type.h>
+#include <minix/vm.h>
+#include <minix/crtso.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/resource.h>
+#include <sys/utsname.h>
+#include <string.h>
+#include <machine/archtypes.h>
+#include <env.h>
+
+#include "kernel/const.h"
+#include "kernel/config.h"
+#include "kernel/proc.h"
+
 /*===========================================================================*
  *				do_semaphore				             *
  *===========================================================================*/
