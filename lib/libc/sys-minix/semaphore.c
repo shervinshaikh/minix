24e4dc8643bc10275a0684cc5e3a65852909a387
Shervin Shaikh <shervinater@gmail.com>
2013-05-06 19:39:21 -0700

    updating make, changing some .h files

--- lib/libc/sys-minix/semaphore.c
+++ lib/libc/sys-minix/semaphore.c
@@ -8,3 +8,17 @@ int semaphore()
 }
 
 // sem_init sem_up
+
+
+// int mycall(a, b, c)
+// int a, b, c;
+// {
+	// message m;
+	//  You may pass a buffer pointer here if necessary,
+	// using m.m1_p1 instead. You can learn from
+	// /usr/src/lib/posix/_write.c 
+	// m.m1_i1 = a;
+	// m.m1_i2 = b;
+	// m.m1_i3 = c;
+	// return(_syscall(FS, MYCALL, &m)); 
+// }
--- servers/semaphore/semaphore.c
+++ servers/semaphore/semaphore.c
@@ -1,66 +1,3 @@
-// #include <minix/keymap.h>
-// #include <minix/callnr.h>
-// #include <minix/com.h>
-// #include <minix/ds.h>
-// #include <minix/type.h>
-// #include <minix/endpoint.h>
-// #include <minix/minlib.h>
-// #include <minix/type.h>
-// #include <minix/vm.h>
-// #include <minix/crtso.h>
-// #include <signal.h>
-// #include <stdlib.h>
-// #include <fcntl.h>
-// #include <sys/resource.h>
-// #include <sys/utsname.h>
-// #include <string.h>
-// #include <machine/archtypes.h>
-// #include <env.h>
-// #include <stdio.h>
-// sef_startup
-/*===========================================================================*
- *				do_semaphore				             *
- *===========================================================================*/
-// int p = 0;
-// int main() {
-//   printf("p: %d\n", p++);
-//   printf("2.0 Adios motherfucker 2.0\n");
-//   return 0;
-// }
-
-
-
-#define _SYSTEM	1
-#define _MINIX 1
-
-// #include <minix/keymap.h>
-// #include <minix/ds.h>
-// #include <minix/minlib.h>
-// #include <minix/vm.h>
-// #include <minix/crtso.h>
-// #include <signal.h>
-// #include <fcntl.h>
-// #include <sys/resource.h>
-// #include <sys/utsname.h>
-// #include <string.h>
-// #include <machine/archtypes.h>
-// #include <env.h>
-
-// #include <minix/callnr.h>
-// #include <minix/com.h>
-// #include <minix/config.h>
-// #include <minix/ipc.h>
-// #include <minix/endpoint.h>
-// #include <minix/sysutil.h>
-// #include <minix/const.h>
-// #include <minix/type.h>
-
-// #include <sys/types.h>
-// #include <time.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <errno.h>
-
 #include "semaphore.h"
 #include <minix/keymap.h>
 #include <minix/callnr.h>
@@ -94,27 +31,28 @@ int main(void)
 {
 
 	printf("Semaphore service is now running..........\n");
-	message m;
-	endpoint_t who_e;
-	int call_type, ipc_status;
+	// because its defined in glo.h
+	//message m;
+	//endpoint_t who_e;
+	int result;
 
 	/* SEF local startup. */
 	sef_startup();
 
 	/* This is SEMAPHORE's main loop-  get work and do it, forever and forever. */
 	while (TRUE) {
-		int r;
+		int ipc_status;
 
 		/* wait for request message */
-		if ((r = sef_receive_status(ANY, &m, ipc_status)) != OK)
+		if ((result = sef_receive_status(ANY, &m, ipc_status)) != OK)
 			printf("SEMAPHORE receive error %d\n", r);
 		who_e = m.m_source;
-		call_type = m.m_type;
+		call_nr = m.m_type;
 
-		printf("Call Type: %d", call_type);
+		printf("Call Number: %d", call_nr);
 		printf("Who sent it: %d\n", who_e);
 
-		r = (*call_vec[call_type])();
+		result = (*call_vec[call_nr])();
 	}
 
 	/* impossible to get here */
