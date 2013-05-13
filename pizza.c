a5b37afbe8cdb21a0c3a87d532ba7e9d90379ba1
ramon.rovirosa@gmail.com <ramonrovirosa@gmail.com>
2013-05-12 19:29:58 -0700

    algorithm for pizzas using forking

--- pizza.c
+++ pizza.c
@@ -1,125 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int semEating = sem_init(1); //1 means no one is eating, free.
 int semUGrad  = sem_init(2); //2 total undergrads
-int semGrad   = sem_init(6); //6 total grad students
+int semGrad   = sem_init(2); //6 total grad students
 
-int pizzaArray[8]={1,2,3,4,5,6,7,8};
-int index =0;
+//int pizzaArray[8]={1,2,3,4,5,6,7,8};
+int numStudents = 8;
 int twoTables=0;
+
+pid_t *childPids = NULL;
+pid_t p;
+
+int prev_student=0;
 
 int main(void){
   //1-6 6 grads
   //7-8 2 ugrads
 
-  int n = sizeof(pizzaArray)/ sizeof(pizzaArray[0]);
-  randomize (PizzaArray, n);
-  printArray(PizzaArray, n);
- 
-  while(1){
-    if(pizzaArray[index] < 7)
-      Grad();
-    else
-      UGrad();
+  int i;
 
+  srand(time(NULL));
+  while(1){
+    for(i=0;i<numStudents;i++){
+      int student = prev_student;
+      while(prev_student == student)
+	(rand() % (numStudents))+1;
+      
+      if( (p=fork()) == 0){//child process
+	if(student < 7)
+	  Grad(student);
+	else
+	  UGrad(student);
+	prev_student=0;
+	exit(0);
+      }
+      else
+	childPids[i]=p;
+    }
+    
   }
   
   return 0;
 }
 
-void UGrad(){
-  sem_down(semEating);//undergrads now eating
-  
-  sem_down(semUGrad);
-  UGradEat();//undergrad1 now eat
-  
-  if(pizzaArray[index] == 7 || pizzaArray[index]==8){
-    sem_down(semUGrad); //only if(next 2 students in queue is ugrad then)
-    twoTables = 1;
-    UGradEat();//undergrad2 now eat
-  }
+void UGrad(int num){
 
-  
-  sem_up(semEating); //done eating
-  if(twoTables == 1){
+  if(prev_student == 7 || prev_student==8){
+    UGrad2(num);
+  }
+  else{
+    sem_down(semEating); 
+    sem_down(semUGrad);
+    prev_student = num;
+    UGradEat(num);
+    //prev_student = num;
+    sem_up(semEating);
     sem_up(semUGrad);
-    twoTables = 0;
   }
-
-  sem_up(semUGrad);  //only (if 2 undergrads eating, not just one)
 }
 
-void UGradEat(){
-  //eating move on to next;
-  if(index == 7)
-    index = 0;
-  else
-    index++
+void UGrad2(int num){
+  sem_down(semUGrad);
+  UGradEat(num);
+  sem_up(semUGrad);
 }
 
-void Grad(){
-  sem_down(semEating);//grads now eating
+void UGradEat(int num){
+  printf("Under Grad %d Eating\n", num-6);
+}
+
+void Grad(int num){
+
+   if(prev_student >= 1 && prev_student <= 6){
+    Grad2(num);
+  }
+   else{
+     sem_down(semEating); 
+     sem_down(semGrad);
+     prev_student = num;
+     GradEat(num);
+     //prev_student = num;
+     sem_up(semEating);
+     sem_up(semGrad);
+   }
   
+}
+
+void Grad2(int num){
   sem_down(semGrad);
-  GradEat();
-  //first table grad now eat pizza
-  if(pizzaArray[index] != 7 &&  pizzaArray[index] != 8) //only (if 2 grads)
-  {
-    sem_down(semGrad);
-    twoTables = 1;
-    GradEat();
-    //second table grad now eat pizza
-  }
-
-  sem_up(semEating);
-
+  GradEat(num);
   sem_up(semGrad);
-  if( twoTables == 1){
-    sem_up(semGrad);
-     twoTables = 0;
-  }
-  
 }
 
-void GradEat(){
+void GradEat(int num){
   //eating move on to next;
-  if(index == 7)
-    index = 0;
-  else
-    index++
+  printf("Grad %d Eating\n", num);
 }
 
-void swap (int *a, int *b)
-{
-  int temp = *a;
-  *a = *b;
-  *b = temp;
-}
- 
-// A utility function to print an array
-void printArray (int arr[], int n)
-{
-  for (int i = 0; i < n; i++)
-    printf("%d ", arr[i]);
-   printf("\n");
-}
- 
-// A function to generate a random permutation of arr[]
-void randomize ( int arr[], int n )
-{
-  // Use a different seed value so that we don't get same
-  // result each time we run this program
-  srand ( time(NULL) );
- 
-  // Start from the last element and swap one by one. We don't
-  // need to run for the first element that's why i > 0
-  for (int i = n-1; i > 0; i--)
-    {
-      // Pick a random index from 0 to i
-      int j = rand() % (i+1);
- 
-      // Swap arr[i] with the element at random index
-      swap(&arr[i], &arr[j]);
-    }
-}
