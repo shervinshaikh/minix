de5387716fb7d0050c4f6ae409c21b97705a61bc
Shervin Shaikh <shervinater@gmail.com>
2013-05-12 19:37:08 -0700

    working on semaphore service and implementing the queue in it

--- queue.c
+++ queue.c
@@ -57,17 +57,17 @@ int queue_size(struct Queue* q){
 	return q->size;
 }
 
-int main(){
-	printf("about to add to queue\n");
-	struct Queue *q = init_queue();
-	enqueue(q, 1);
-	enqueue(q, 2);
-	enqueue(q, 3);
-	printf("%d was removed from the queue\n", dequeue(q));
-	printf("Current queue size: %d\n", queue_size(q));
-	printf("%d was removed from the queue\n", dequeue(q));
-	enqueue(q, 4);
-	printf("%d was removed from the queue\n", dequeue(q));
-	printf("%d was removed from the queue\n", dequeue(q));
-	return 0;
-}
+// int main(){
+// 	printf("about to add to queue\n");
+// 	struct Queue *q = init_queue();
+// 	enqueue(q, 1);
+// 	enqueue(q, 2);
+// 	enqueue(q, 3);
+// 	printf("%d was removed from the queue\n", dequeue(q));
+// 	printf("Current queue size: %d\n", queue_size(q));
+// 	printf("%d was removed from the queue\n", dequeue(q));
+// 	enqueue(q, 4);
+// 	printf("%d was removed from the queue\n", dequeue(q));
+// 	printf("%d was removed from the queue\n", dequeue(q));
+// 	return 0;
+// }
